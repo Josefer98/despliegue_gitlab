1afd61d0095cf4814f2e454f44244b6f37c1e0eb
Alexander <sacacaalexander.159@gmail.com>
2024-04-28 11:42:12 -0400

    plantilla inicial

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+//Route::get('/', function () {return view('welcome');});
+
+
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/', [AdminController::class, 'index'])->name('admin.index');//->middleware('auth');
