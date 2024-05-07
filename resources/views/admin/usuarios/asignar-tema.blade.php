1335a083dfc8c4cd4be7f59d5f934d67f9b14834
unknown <sacacaalexander.159@gmail.com>
2024-05-07 08:38:57 -0400

    haciend cambios de layout

--- resources/views/admin/usuarios/asignar-tema.blade.php
+++ resources/views/admin/usuarios/asignar-tema.blade.php
@@ -1,13 +1,15 @@
+<!-- resources/views/admin/usuarios/asignar-tema.blade.php -->
+
 @extends('layouts.admin')
 
 @section('content')
     <h2>Asignar Tema de Grado a {{ $usuario->name }}</h2>
     <form action="{{ route('usuarios.asignar-tema-action', $usuario->id) }}" method="POST">
         @csrf
-        <select name="tema" id="tema" class="form-control">
+        <select name="tema_id" id="tema_id" class="form-control">
             <option value="" selected disabled>Seleccionar Tema de Grado</option>
             @foreach ($temasDisponibles as $tema)
-                <option value="{{ $tema }}">{{ $tema }}</option>
+                <option value="{{ $tema->id }}">{{ $tema->titulo }}</option>
             @endforeach
         </select>
         <button type="submit" class="btn btn-primary mt-2">Asignar Tema</button>
